873c4e3dec248f5ad59811e48c43c1be099a4c43
dudu <hellodudu86@gmail.com>
2020-11-19 17:53:44 +0800

    linux 下启动脚本更新

--- start_linux.sh
+++ start_linux.sh
@@ -5,6 +5,9 @@ docker-compose down
 echo "启动consul容器..."
 docker-compose up -d
 
+echo "等待5秒consul初始化结束"
+sleep 5
+
 echo "导入服务配置..."
 curl --request PUT --data @StartSceneConfig.txt http://localhost:8500/v1/kv/service_config
 
